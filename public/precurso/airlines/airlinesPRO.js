9aa3bb18b58ed565544087c79234fbddd5213ce0
Andrea Martinez <andrea.maort@gmail.com>
2022-07-13 21:48:24 +0200

    airlines refactored done

--- public/precurso/airlines/airlinesPRO.js
+++ public/precurso/airlines/airlinesPRO.js
@@ -1,5 +1,3 @@
-// Punto 1)
-
 const usuario = prompt("Bienvenido a Skylab Airlines. ¿Cuál es tu nombre?");
 
 console.log(`Bienvenido ${usuario}`);
@@ -7,87 +5,28 @@ console.log(`Bienvenido ${usuario}`);
 // Enunciado
 
 let flights = [
-  { id: 00, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
+  { id: 0, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
 
-  { id: 01, to: "New York", from: "Barcelona", cost: 700, scale: false },
+  { id: 1, to: "New York", from: "Barcelona", cost: 700, scale: false },
 
-  { id: 02, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
+  { id: 2, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
 
-  { id: 03, to: "Paris", from: "Barcelona", cost: 210, scale: false },
+  { id: 3, to: "Paris", from: "Barcelona", cost: 210, scale: false },
 
-  { id: 04, to: "Roma", from: "Barcelona", cost: 150, scale: false },
+  { id: 4, to: "Roma", from: "Barcelona", cost: 150, scale: false },
 
-  { id: 05, to: "London", from: "Madrid", cost: 200, scale: false },
+  { id: 5, to: "London", from: "Madrid", cost: 200, scale: false },
 
-  { id: 06, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
+  { id: 6, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
 
-  { id: 07, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
+  { id: 7, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
 
-  { id: 08, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
+  { id: 8, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
 
-  { id: 09, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
+  { id: 9, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
 
   { id: 10, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
 ];
-
-//AIRLINES PRO
-
-const adminPRO = () => {
-  let accion;
-
-  do {
-    accion = prompt("Que quieres hacer? crear o eliminar vuelos?");
-
-    if (accion) accion = accion.toLowerCase();
-  } while (accion !== "crear" && accion !== "eliminar" && accion !== null);
-
-  if (accion === null) {
-    alert(`Que tengas un buen día ${usuario}`);
-    return;
-  } else {
-    switch (accion) {
-      case "crear":
-        if (flights.length < 15) {
-          crearVuelo();
-          break;
-        } else {
-          alert(
-            "Ya has alcanzado " + flights.length + ", ya no puedes añadir mas."
-          );
-          break;
-        }
-      case "eliminar":
-        mostrarVuelos(flights);
-        const id = +prompt(
-          "¿Que vuelo desear eliminar? Ingrese un número de ID"
-        );
-        const nuevaArrayVuelosMenosUno = flights.filter(
-          (vuelo) => vuelo.id !== id
-        );
-        flights = nuevaArrayVuelosMenosUno;
-        mostrarVuelos(flights);
-        break;
-      default:
-        alert("No existe esta opción, asegurese de escribir CREAR o ELIMINAR");
-    }
-  }
-  adminPRO();
-};
-
-const crearVuelo = () => {
-  const nuevoVuelo = {};
-  nuevoVuelo.id = flights.length;
-  nuevoVuelo.to = prompt("Destino?");
-  nuevoVuelo.from = prompt("Salida?");
-  nuevoVuelo.cost = +prompt("Coste?");
-  nuevoVuelo.scale = confirm(
-    "Tiene escala? Pulsa ACEPTAR si tienes escala o CANCELAR si no tiene escala"
-  );
-
-  flights.push(nuevoVuelo);
-
-  mostrarVuelos(flights);
-};
 
 const mostrarVuelos = (array) => {
   console.clear();
@@ -102,6 +41,59 @@ const mostrarVuelos = (array) => {
       );
     }
   }
+};
+
+const crearVuelo = () => {
+  const nuevoVuelo = {};
+  nuevoVuelo.id = flights.length;
+  nuevoVuelo.to = prompt("Destino?");
+  nuevoVuelo.from = prompt("Salida?");
+  nuevoVuelo.cost = +prompt("Coste?");
+  // eslint-disable-next-line no-restricted-globals
+  nuevoVuelo.scale = confirm(
+    "Tiene escala? Pulsa ACEPTAR si tienes escala o CANCELAR si no tiene escala"
+  );
+
+  flights.push(nuevoVuelo);
+
+  mostrarVuelos(flights);
+};
+
+const adminPRO = () => {
+  let accion;
+
+  do {
+    accion = prompt("Que quieres hacer? crear o eliminar vuelos?");
+
+    if (accion) accion = accion.toLowerCase();
+  } while (accion !== "crear" && accion !== "eliminar" && accion !== null);
+
+  if (accion === null) {
+    alert(`Que tengas un buen día ${usuario}`);
+    return;
+  }
+  switch (accion) {
+    case "crear":
+      if (flights.length < 15) {
+        crearVuelo();
+        break;
+      } else {
+        alert(`Ya has alcanzado ${flights.length} ya no puedes añadir mas.`);
+        break;
+      }
+    case "eliminar":
+      mostrarVuelos(flights);
+      const id = +prompt("¿Que vuelo desear eliminar? Ingrese un número de ID");
+      const nuevaArrayVuelosMenosUno = flights.filter(
+        (vuelo) => vuelo.id !== id
+      );
+      flights = nuevaArrayVuelosMenosUno;
+      mostrarVuelos(flights);
+      break;
+    default:
+      alert("No existe esta opción, asegurese de escribir CREAR o ELIMINAR");
+  }
+  adminPRO();
 };
 
 function User() {
@@ -134,13 +126,14 @@ function airlinesPro() {
   let pregunta;
 
   do {
-    let preguntaUser = prompt(`Hola ${usuario}, ¿eres ADMIN o USER?`);
+    const preguntaUser = prompt(`Hola ${usuario}, ¿eres ADMIN o USER?`);
     pregunta = preguntaUser.toLowerCase();
 
     if (pregunta === "admin") {
       adminPRO();
       return;
-    } else if (pregunta === "user") {
+    }
+    if (pregunta === "user") {
       User();
       return;
     }
--- public/precurso/calculator/calculatorSimple.js
+++ public/precurso/calculator/calculatorSimple.js
@@ -11,54 +11,61 @@ let restaTotal;
 let diviTotal;
 let multiplicaTotal;
 
-if (Number.isNaN(number1) || (Number.isNaN(number2) && numberUser2 !== null)) {
-  // eslint-disable-next-line no-throw-literal
-  throw "Error, el campo debe ser un valor numérico";
+function calculator() {
+  if (
+    Number.isNaN(number1) ||
+    (Number.isNaN(number2) && numberUser2 !== null)
+  ) {
+    // eslint-disable-next-line no-throw-literal
+    throw "Error, el campo debe ser un valor numérico";
+  }
+
+  // damos la raíz cuadrada cuando se mete solo el primer número y el segundo se da a cancelar
+  else if (number1 > 0 && numberUser2 === null) {
+    const raiz = Math.sqrt(parseFloat(number1));
+
+    console.log(`La raíz cuadrada de ${number1} es ${raiz}`);
+  } else {
+    const suma = (variable1, variable2) => variable1 + variable2;
+    sumaTotal = suma(number1, number2);
+
+    const resta = (variable1, variable2) => variable1 - variable2;
+    restaTotal = resta(number1, number2);
+
+    const multiplicar = (variable1, variable2) => variable1 * variable2;
+    multiplicaTotal = multiplicar(number1, number2);
+
+    const dividir = (variable1, variable2) => variable1 / variable2;
+    diviTotal = dividir(number1, number2);
+  }
+
+  if (Number.isInteger(sumaTotal)) {
+    results.push(sumaTotal);
+  } else {
+    results.push(sumaTotal.toFixed(3));
+  }
+
+  if (Number.isInteger(restaTotal)) {
+    results.push(restaTotal);
+  } else {
+    results.push(restaTotal.toFixed(3));
+  }
+
+  if (Number.isInteger(multiplicaTotal)) {
+    results.push(multiplicaTotal);
+  } else {
+    results.push(multiplicaTotal.toFixed(3));
+  }
+
+  if (Number.isInteger(diviTotal)) {
+    results.push(diviTotal);
+  } else {
+    results.push(diviTotal.toFixed(3));
+  }
+
+  console.log(
+    `La suma de los número es igual a ${results[0]}, la resta es ${results[1]}, la multiplicación es ${results[2]}, la división es ${results[3]}`
+  );
 }
 
-// damos la raíz cuadrada cuando se mete solo el primer número y el segundo se da a cancelar
-else if (number1 > 0 && numberUser2 === null) {
-  const raiz = Math.sqrt(parseFloat(number1));
-
-  console.log(`La raíz cuadrada de ${number1} es ${raiz}`);
-} else {
-  const suma = (variable1, variable2) => variable1 + variable2;
-  sumaTotal = suma(number1, number2);
-
-  const resta = (variable1, variable2) => variable1 - variable2;
-  restaTotal = resta(number1, number2);
-
-  const multiplicar = (variable1, variable2) => variable1 * variable2;
-  multiplicaTotal = multiplicar(number1, number2);
-
-  const dividir = (variable1, variable2) => variable1 / variable2;
-  diviTotal = dividir(number1, number2);
-}
-
-if (Number.isInteger(sumaTotal)) {
-  results.push(sumaTotal);
-} else {
-  results.push(sumaTotal.toFixed(3));
-}
-
-if (Number.isInteger(restaTotal)) {
-  results.push(restaTotal);
-} else {
-  results.push(restaTotal.toFixed(3));
-}
-
-if (Number.isInteger(multiplicaTotal)) {
-  results.push(multiplicaTotal);
-} else {
-  results.push(multiplicaTotal.toFixed(3));
-}
-
-if (Number.isInteger(diviTotal)) {
-  results.push(diviTotal);
-} else {
-  results.push(diviTotal.toFixed(3));
-}
-
-console.log(
-  `La suma de los número es igual a ${results[0]}, la resta es ${results[1]}, la multiplicación es ${results[2]}, la división es ${results[3]}`
-);
+calculator();
